e85a3572d5c5ecdfac00d7d5b3f80226fc4b74e7
YilouWang <wangyilou123@gmail.com>
2024-07-04 14:54:11 +0200

    Enable RedOr conditional constraint

---
+++ constraint_random/conditional_constraint/verilator_sim/run_commands.py
@@ -0,0 +1,25 @@
+import os
+"""
+def run_command(command):
+    try:
+        print(f"Running command: {command}")
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(result.stdout.decode())
+    except subprocess.CalledProcessError as e:
+        print(f"Error running command: {command}")
+        print(e.stderr.decode())
+        raise
+"""
+def main():
+    commands = [
+        "cd /home/yilou/Desktop/OSVISE/verilator && ./configure && make",
+        "make clean",
+        "make json_dump=1 debug=1"
+    ]
+
+    for command in commands:
+        print(f"**************************\nRunning command: {command}\n**************************")
+        os.system(command)
+
+if __name__ == '__main__':
+    main() 
